097457785b3f9f8e37cae0079e4e1cc518cf4be2
Brian McIlwain <brian.mcilwian@technicallyemployed.com>
2019-06-14 16:55:56 -0700

    Set up swagger

--- routes/leads.js
+++ routes/leads.js
@@ -3,7 +3,23 @@ var router = express.Router();
 const Lead = require('../models/lead.js');  
 
  
-/* POST lead */
+/**
+ * @swagger
+ * /leads:
+ *    post:
+ *      description: This saves the contact info for a lead
+ *      produces:
+ *        - application/json
+ *      parameters:
+ *        - name: email
+ *          description: Email to register.
+ *          in: path
+ *          required: true
+ *          type: string
+ *      responses:
+ *        200:
+ *          description: Lead saved
+ */
 router.post('/:email', function(req, res, next) {
 	let newLead = new Lead()
 
